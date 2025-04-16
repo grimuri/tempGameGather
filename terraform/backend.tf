f11e89ecb29c340c31158c4d2e43c98250c60541
Jakub Popiolek <grimurin@gmail.com>
2025-04-16 16:58:16 +0200

    Commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "gamegather-rg"
-    storage_account_name = "gamegathersa"
+    resource_group_name  = "pr-gamegather-rg"
+    storage_account_name = "gamegatherst"
     container_name       = "dev-tfstate"
     key                 = "dev.terraform.tfstate"
     
