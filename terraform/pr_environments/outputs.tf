56659338e61e2d34a2970f7a273845466ae677b7
Jakub Popiolek <grimurin@gmail.com>
2025-04-25 11:56:48 +0200

    Commit

---
+++ terraform/pr_environments/outputs.tf
@@ -0,0 +1,14 @@
+output "webapp_name" {
+  value = azurerm_linux_web_app.as.name
+  description = "Nazwa aplikacji webowej"
+}
+
+output "webapp_publish_url" {
+  value = "https://${azurerm_linux_web_app.as.default_hostname}"
+  description = "URL publikacji aplikacji webowej"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+  description = "Nazwa grupy zasobów"
+}
