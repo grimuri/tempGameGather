8a8af14aa9db4286d859ad1eafee329a87b24685
Jakub Popiolek <grimurin@gmail.com>
2025-04-29 18:49:43 +0200

    Comit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,11 @@
-variable "prefix" {
+variable "prefix_api" {
   default = "gamegather-api"
+}
+
+variable "prefix_db" {
+  default = "gamegather-db"  
 }
 
 variable "location" {
   default = "polandcentral"
-  
 }
